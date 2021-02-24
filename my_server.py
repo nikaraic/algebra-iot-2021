6fb43982961f6968e899a48292525d4e4edec0c0
nikaraic <nikaraic@fer.hr>
2021-02-24 18:32:32 +0100

    Add books dictionary

--- my_server.py
+++ my_server.py
@@ -1,7 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+books = [{'Name': 'Snow White', 'Author': 'Grimm brothers'},
+         {'Name': "Alice's Adventures in Wonderland", 'Author': 'Lewis Carrol'}
+         ]
+
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello world!'
 
