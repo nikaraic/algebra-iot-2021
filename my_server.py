821bd9a5ba4dab0ee390a795159bb5ac2c8c13bd
nikaraic <nikaraic@fer.hr>
2021-02-24 18:42:54 +0100

    Minor fix

--- my_server.py
+++ my_server.py
@@ -1,4 +1,7 @@
+#!usr/bin/python3
 from flask import Flask
+import jsonify
+
 app = Flask(__name__)
 
 books = [{'name': 'Snow White', 'author': 'Grimm brothers'},
@@ -9,10 +12,10 @@ books = [{'name': 'Snow White', 'author': 'Grimm brothers'},
 def hello_world():
     return 'Hello world!'
 
-@app.route("/api/books/titles", methods=['GET'])
+@app.route("/api/books", methods=['GET'])
 def return_all():
     return jsonify({'books': books})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
 
