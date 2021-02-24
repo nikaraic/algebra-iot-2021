d6e58a02416673e309dc9175c76083c34304ba5a
nikaraic <nikaraic@fer.hr>
2021-02-24 18:38:15 +0100

    Add return_all function

--- my_server.py
+++ my_server.py
@@ -1,13 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-books = [{'Name': 'Snow White', 'Author': 'Grimm brothers'},
-         {'Name': "Alice's Adventures in Wonderland", 'Author': 'Lewis Carrol'}
+books = [{'name': 'Snow White', 'author': 'Grimm brothers'},
+         {'name': "Alice's Adventures in Wonderland", 'author': 'Lewis Carrol'}
          ]
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello world!'
+
+@app.route("api/books/titles", methods=['GET'])
+def return_all():
+    return jsonify({'books': books})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
