2bf29f46082a6b240e2843f957bac6e286333a0a
nikaraic <nikaraic@fer.hr>
2021-02-24 19:14:49 +0100

    Add database

--- my_server.py
+++ my_server.py
@@ -1,8 +1,17 @@
 #!usr/bin/python3
 from flask import Flask
 import jsonify
+from flask_mysqldb import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+
+#MySQL configuration
+app.config['MYSQL_USER'] = 'booksuser'
+app.config['MYSQL_PASSWORD'] = 'booksuser123'
+app.config['MYSQL_DB'] = 'booksDB'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql.init_app(app)
 
 books = [{'name': 'Snow White', 'author': 'Grimm brothers'},
          {'name': "Alice's Adventures in Wonderland", 'author': 'Lewis Carrol'}
@@ -15,6 +24,13 @@ def hello_world():
 @app.route("/api/books", methods=['GET'])
 def return_all():
     return jsonify({'books': books})
+
+@app.route("/api/books/titles", methods=['GET'])
+def return_titles():
+    titles = []
+    for book in books:
+        titles.append(book['name'])
+    return jsonify({'titles': titles})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
